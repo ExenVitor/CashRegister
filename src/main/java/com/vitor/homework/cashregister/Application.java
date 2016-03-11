f319bc3c24b299bfa86f65322ca00a31116e969a
Vitor Chen <exen3995@gmail.com>
2016-03-11 23:51:47 +0800

    initial commit

---
+++ src/main/java/com/vitor/homework/cashregister/Application.java
@@ -0,0 +1,13 @@
+package com.vitor.homework.cashregister;
+
+/**
+ * Created by Vitor Chen on 16-3-11.
+ * mail: exen3995@gmail.com
+ */
+
+public class Application {
+
+    public static void main(String []args){
+
+    }
+}
