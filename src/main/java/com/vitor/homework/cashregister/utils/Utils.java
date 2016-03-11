862e06044f2bf95f8ea936bac7eb13bcfc1fdb72
Vitor Chen <exen3995@gmail.com>
2016-03-12 01:48:25 +0800

    add empty string verify util

---
+++ src/main/java/com/vitor/homework/cashregister/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.vitor.homework.cashregister.utils;
+
+/**
+ * Created by Vitor Chen on 16-3-12.
+ * mail: exen3995@gmail.com
+ */
+
+public class Utils {
+
+    public static boolean isStringEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+}
